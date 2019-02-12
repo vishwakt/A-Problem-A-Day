ca1a2e53d29c52a100e25dd1a2e368adbc02fcda
Vishwak <vishwakt@gmail.com>
2019-02-12 00:38:18 -0800

    solution

--- listElementsAddUptoK.py
+++ listElementsAddUptoK.py
@@ -2,3 +2,17 @@
 #
 # For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
+def addUptoK(listOfNumbers, k):
+    # Create a hashmap / dictionary of all the numbers in the list
+    dictOfNumbers = {}
+    for i in listOfNumbers:
+        dictOfNumbers[i] = i
+
+    # For every key in the dictionary check if a key2 with the value of K minus key1 exists
+    for key in dictOfNumbers.keys():
+        print key
+        if k-key in dictOfNumbers.keys():
+            print key, k-key
+            return True
+
+print(addUptoK([1,2,3, 8, 9, 11, 12], 21))
