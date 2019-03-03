06e4f18687d3ff13d08ff66774f8eff20975eb0a
Vishwak <vishwakt@gmail.com>
2019-03-02 23:42:26 -0800

    Solution

---
+++ 155_min_stack.py
@@ -0,0 +1,42 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        if self.stack:
+            self.stack.append(min(self.stack[-2], x))
+        else:
+            self.stack.append(x)
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if self.stack:
+            self.stack.pop()
+            self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self.stack:
+            return self.stack[-1]
+
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.stack:
+            return self.stack[-2]
+
