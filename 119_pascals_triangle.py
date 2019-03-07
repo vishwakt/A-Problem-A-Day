3da68585a1e2c902458853b06efbc1834efe82f5
Vishwak <vishwakt@gmail.com>
2019-03-06 18:19:25 -0800

    Solution

---
+++ 119_pascals_triangle.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        row = [1]
+        for _ in range(rowIndex):
+            # print zip([0] + row, row + [0])
+            row = [x + y for x, y in zip([0] + row, row + [0])]
+            # print row
+        return row
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.getRow(5)
