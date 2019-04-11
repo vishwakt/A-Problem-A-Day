e3c127e4618e1faad1a5a1f9d8baffe23dbde3e4
Vishwak <vishwakt@gmail.com>
2019-04-11 11:59:08 -0400

    Solution

---
+++ 13_roman_to_integer.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        values = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        sum = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+
+        for char in s:
+            sum += values[char]
+        return sum
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.romanToInt('LVIII')
