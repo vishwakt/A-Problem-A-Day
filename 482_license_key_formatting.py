0a0e5516bdb73c2021bab09b8297da4f614ddb3a
Vishwak <vishwakt@gmail.com>
2019-04-12 10:48:10 -0400

    Solution

---
+++ 482_license_key_formatting.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def licenseKeyFormatting(self, S, K):
+        """
+        :type S: str
+        :type K: int
+        :rtype: str
+        """
+        strip_dash = S.replace('-', "").upper()
+        lenchars = len(strip_dash)
+        if lenchars % K != 0:
+            len_first = lenchars % K
+        else:
+            len_first = K
+
+        res = strip_dash[:len_first] + "-"
+
+        for i in range(len_first, len(strip_dash), K):
+            res += strip_dash[i: i + K] + '-'
+
+        return res[:-1]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.licenseKeyFormatting("5F3Z-2e-9-wa", 4)
