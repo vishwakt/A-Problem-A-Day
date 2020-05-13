b7935848691c5b8b701cb6dffcad8c659a948d2f
Vishwak <vishwakt@gmail.com>
2020-05-12 23:29:51 -0400

    Solution

---
+++ 572_subtree_of_another_tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution(object):
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
+
+
+if __name__ == "__main__":
+    s = Solution()
