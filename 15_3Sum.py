10893b8f3d596daca247570b7a8e2737151760c7
Vishwak <vishwakt@gmail.com>
2020-05-16 21:28:31 -0400

    Solution

---
+++ 15_3Sum.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        length = len(nums)
+        nums.sort()
+        result = []
+
+        for i in range(length - 2):
+            if nums[i] > 0:
+                break
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            l, r = i + 1, length - 1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+
+                if total < 0:
+                    l += 1
+
+                elif total > 0:
+                    r -= 1
+
+                else:
+                    result.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+
+                    l += 1
+                    r -= 1
+
+        return result
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.threeSum(nums=[-1, 0, 1, 2, -1, -4]))
