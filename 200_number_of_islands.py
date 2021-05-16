127a553b08e4b34675cc978a76c6a3656b3a0a12
Vishwak <vishwakt@gmail.com>
2021-05-15 22:36:51 -0400

    200 solution in python3

--- 200_number_of_islands.py
+++ 200_number_of_islands.py
@@ -1,28 +1,33 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
-            return 0
-        count = 0
+            return
+        numberOfIslands = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
                     self.dfs(grid, i, j)
-                    count += 1
-        return count
+                    numberOfIslands += 1
+        return numberOfIslands
 
     def dfs(self, grid, i, j):
-        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
             return
-        grid[i][j] = "#"
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i-1, j)
+        grid[i][j] = '#'
         self.dfs(grid, i, j+1)
         self.dfs(grid, i, j-1)
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
 
 
 if __name__ == "__main__":
     s = Solution()
+    print(s.numIslands(grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]))
