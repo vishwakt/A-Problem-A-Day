0a1099fd043d2843d45d03eefe19284c987d9c90
Vishwak <vishwakt@gmail.com>
2021-05-16 17:19:52 -0400

    238 Solution python3

--- 238_product_of_array_except_self.py
+++ 238_product_of_array_except_self.py
@@ -1,25 +1,25 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        output = [1] * len(nums)
+from typing import List
 
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        output = [1] * len(nums)
         prod = 1
 
         for i in range(len(nums)):
             output[i] *= prod
             prod *= nums[i]
+        # print(leftProd)
 
         prod = 1
-        for i in range(len(nums) - 1, -1 , -1):
+        for i in range(len(nums)-1, -1, -1):
             output[i] *= prod
             prod *= nums[i]
 
-        return output
+        print(output)
 
 
 if __name__ == "__main__":
     s = Solution()
-    print s.productExceptSelf([1,2,3,4])
+    print(s.productExceptSelf([1,2,3,4]))
