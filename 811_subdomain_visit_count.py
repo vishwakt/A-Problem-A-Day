ff7783b5bfd9963ed585dc0449964d153fc56f50
Vishwak <vishwakt@gmail.com>
2021-05-16 23:49:44 -0400

    811 Solution python3

--- 811_subdomain_visit_count.py
+++ 811_subdomain_visit_count.py
@@ -1,22 +1,20 @@
-class Solution(object):
-    def subdomainVisits(self, cpdomains):
-        """
-        :type cpdomains: List[str]
-        :rtype: List[str]
-        """
+from typing import List
+
+
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
         dict = {}
-        for data in cpdomains:
-            count, subdomain = data.split(" ")
-            # print count, subdomain
-            subs = subdomain.split('.')
-            # print subs
+        for domain in cpdomains:
+            count, domain = domain.split(' ')
+            subs = domain.split('.')
+            # for subdomain in domain:
             for i in range(len(subs)):
                 sub = ".".join(subs[i:])
                 dict[sub] = dict.get(sub, 0) + int(count)
-        list_ = [' '.join([str(val), key]) for key, val in dict.items()]
+        list_ = [" ".join([str(val), key]) for key, val in dict.items()]
         return list_
 
 
 if __name__ == "__main__":
     s = Solution()
-    print s.subdomainVisits(["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"])
+    print(s.subdomainVisits(["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]))
