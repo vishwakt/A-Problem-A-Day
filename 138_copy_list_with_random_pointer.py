2e99c42533781067be50d316c87abe94fbc6ecbf
Vishwak <vishwakt@gmail.com>
2021-05-30 19:07:04 -0400

    138 Solution in python3

--- 138_copy_list_with_random_pointer.py
+++ 138_copy_list_with_random_pointer.py
@@ -6,36 +6,36 @@ class Node:
         self.next = next
         self.random = random
 """
+
+
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
 
+
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        print(head)
-        if not head:
-            return None
+        oldToCopy = { None: None }
 
-        curr = head
-        copyOfLL = {}
+        cur = head
 
-        while curr:
-            copyOfLL[curr] = Node(curr.val, None, None)
-            curr = curr.next
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
 
-        curr = head
-        while curr:
-            if curr.random:
-                copyOfLL[curr].random = copyOfLL[curr.random]
-            if curr.next:
-                copyOfLL[curr].next = copyOfLL[curr.next]
-            curr = curr.next
+        cur = head
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
+            cur = cur.next
 
-        return copyOfLL[head]
+        return oldToCopy[head]
 
 
 if __name__ == "__main__":
     s = Solution()
-    s.copyRandomList(head=[[7,None],[13,0],[11,4],[10,2],[1,0]])
+    s.copyRandomList(head = [[7,None],[13,0],[11,4],[10,2],[1,0]])
