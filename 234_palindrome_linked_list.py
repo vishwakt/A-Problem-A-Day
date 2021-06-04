bd6509fb4938a97aae987be7b35a27a86ea3890e
Vishwak <vishwakt@gmail.com>
2021-06-04 06:59:35 -0400

    234 Solution in python3

--- 234_palindrome_linked_list.py
+++ 234_palindrome_linked_list.py
@@ -1,71 +1,44 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
-class Solution(object):
-    def __init__(self):
-        self.head = None
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        fast = head
+        slow = head
 
-    def add_first(self, new_data):
-        """Inserts new node at the beginning of the linked list"""
-        new_node = ListNode(new_data)
-        new_node.next = self.head
-        self.head = new_node
+        # find the midddle of the linked list
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
 
-    def add_last(self, new_data):
-        """Inserts new node at the end of the linked list"""
-        new_node = ListNode(new_data)
-
-        if self.head is None:
-            self.head = new_node
-            return
-
-        last = self.head
-        while last.next:
-            last = last.next
-
-        last.next = new_node
-
-    def isPalindrome(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-        head1 = head
-        head2, count = self.reverse_list(head)
-        flag = False
-        while head2 and head1:
-            if head2.val is head1.val:
-                head2 = head2.next
-                head1 = head1.next
-            else:
-                return False
-        return True
-
-    def reverse_list(self, head):
-        curr = head
+        # reverse second half of linked list
         prev = None
-        next = None
-        count = 0
-        while curr:
-            count += 1
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-        curr = prev
-        return curr, count
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+
+        # check palindrome
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
 
 
 if __name__ == "__main__":
     s = Solution()
-    s.add_first(1)
-    s.add_last(2)
-    s.add_last(2)
-    s.add_last(1)
+    # s.add_first(1)
+    # s.add_last(2)
+    # s.add_last(2)
+    # s.add_last(1)
     # s.add_last(-129)
     # s.add_last(-129)
-    print s.isPalindrome(s.head)
+    print(s.isPalindrome(s.head))
